cec3233899d64ec70332fedfc9fb5549a6803955
ashiqYousuf <ashiqhussainkumar143@gmail.com>
2024-02-26 00:44:24 +0530

    Seperated routes

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -10,7 +10,7 @@ func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	// app.errorLog.Println(trace)
 	app.errorLog.Output(2, trace)
-	http.Error(w, trace, http.StatusInternalServerError)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
 func (app *application) clientError(w http.ResponseWriter, status int) {
